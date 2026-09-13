8b9691092025c066ebf1e608106b3ca57e126249
uta1508 <utaro.9503@gmail.com>
2026-09-13 17:03:20 +0900

    chore: gitignoreを更新し、不要なcsvやsqlファイルを除外

--- fix_profiles_cascade.sql
+++
@@ -1,9 +0,0 @@
--- profilesテーブルのauth.usersへの紐付けもCASCADEに修正する
-ALTER TABLE public.profiles
-  DROP CONSTRAINT IF EXISTS profiles_id_fkey;
-
-ALTER TABLE public.profiles
-  ADD CONSTRAINT profiles_id_fkey
-  FOREIGN KEY (id)
-  REFERENCES auth.users(id)
-  ON DELETE CASCADE;
